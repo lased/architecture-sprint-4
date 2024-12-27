771108ac5dd03affb63e4ee34435926e31c82392
lased <61lased61@gmail.com>
2024-12-27 15:10:22 +0300

    add tracing

--- services/app/index.js
+++ services/app/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const { startServer } = require('./promServer');
 const promClient = require('prom-client');
+const { tracer } = require('./tracing-setup');
 const app = express();
 
 const port = 3000;
 
 promClient.collectDefaultMetrics();
 
+app.use((_, __, next) => {
+  const span = tracer.startSpan('testMiddleware');
+
+  span.setAttributes({ key: 'testMiddleware' });
+
+  process.stdout.write('{"key":"testMiddleware"}\n');
+
+  setTimeout(() => {
+    next();
+    span.end();
+  }, 100);
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World\n');
+  const span = tracer.startSpan('helloWorld');
+
+  process.stdout.write('{"key":"helloWorld"}\n');
+  span.setAttributes({ key: 'helloWorld' });
+  res.send('Hello World from app 1\n');
+  span.end();
 });
 
 app.listen(port, () => {
